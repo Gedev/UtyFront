48f4b903e00550506c4480cb66db321e569202ee
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-12-14 21:01:04 +0100

    Date picker : set rules (min, max)

--- src/app/components/classroom-reservation/classroom-reservation.component.ts
+++ src/app/components/classroom-reservation/classroom-reservation.component.ts
@@ -10,11 +10,9 @@ import {RoomType} from "../../models/room-type";
   styleUrls: ['./classroom-reservation.component.css']
 })
 export class ClassroomReservationComponent implements OnInit {
-
+  startDate: Date;
   minDate: Date;
   maxDate: Date;
-  hours: number[];
-  minutes: number[];
   equipments: RoomEquipment[] = [];
   roomTypes: RoomType[] = [];
   form: FormGroup;
@@ -23,13 +21,12 @@ export class ClassroomReservationComponent implements OnInit {
 
   constructor(private _snackBar: MatSnackBar,
               private formBuilder: FormBuilder) {
-
-    this.hours = Array(11).fill(0, 0, 19).map((x, i) => i);
-    this.minutes = Array(60).fill(0).map((x, i) => i);
     const currentYear = new Date().getFullYear();
-    this.minDate = new Date(currentYear - 20, 0, 1);
-    this.maxDate = new Date(currentYear + 1, 11, 31);
-
+    const currentMonth = new Date().getMonth();
+    const currentDay = new Date().getDate();
+    this.minDate = new Date(currentYear, currentMonth, currentDay);
+    this.maxDate = new Date(currentYear + 1, currentMonth, 31);
+    this.startDate = new Date(currentYear, currentMonth, currentDay);
     this.form = this.formBuilder.group({
       roomTypeId: ['', Validators.required],
       date: ['', Validators.required],
